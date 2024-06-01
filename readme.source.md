8cc20c275933e8afbb833c82157e8552776bb699
Cam MacFarland <distantcam@gmail.com>
2024-06-01 12:46:06 +0800

    Updated readme

---
+++ readme.source.md
@@ -0,0 +1,26 @@
+# EndpointGenerator
+
+[![Build Status](https://img.shields.io/github/actions/workflow/status/distantcam/EndpointGenerator/build.yml)](https://github.com/distantcam/EndpointGenerator/actions/workflows/build.yml)
+[![NuGet Status](https://img.shields.io/nuget/v/EndpointGenerator.svg)](https://www.nuget.org/packages/EndpointGenerator/)
+[![Nuget Downloads](https://img.shields.io/nuget/dt/EndpointGenerator.svg)](https://www.nuget.org/packages/EndpointGenerator/)
+
+
+EndpointGenerator is a Roslyn Source Generator to add methods to call marked minimal api methods.
+
+toc
+
+## NuGet packages
+
+https://nuget.org/packages/EndpointGenerator/
+
+### Your code
+
+snippet: Basic
+
+### What gets generated
+
+snippet: BasicGeneratedCode
+
+### How to use
+
+snippet: Usage
---
+++ src/EndpointGenerator.Example/BasicExamples.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+
+#region Basic
+public static class Basic
+{
+    [EndpointGenerator.EndpointBuilder]
+    public static void Simple(Microsoft.AspNetCore.Routing.IEndpointRouteBuilder builder) =>
+        builder.MapGet("/simple", () => TypedResults.Ok("Hello World"));
+
+    [EndpointGenerator.EndpointBuilder]
+    public static void Grouped(Microsoft.AspNetCore.Routing.RouteGroupBuilder builder) =>
+        builder.MapGet("/grouped", () => TypedResults.Ok("Hello World"));
+
+    [EndpointGenerator.EndpointGroupBuilder("prefix")]
+    public static void GroupedWithPrefix(Microsoft.AspNetCore.Routing.RouteGroupBuilder builder) =>
+        builder.MapGet("/grouped", () => TypedResults.Ok("Hello World"));
+}
+#endregion
+
+public static class Generated
+{
+    #region BasicGeneratedCode
+
+    // The name will be "Map[AssemblyName]Endpoints"
+    public static IEndpointRouteBuilder MapEndpointGeneratorExampleEndpoints(this IEndpointRouteBuilder builder)
+    {
+        Basic.Simple(builder);
+        var group = builder.MapGroup("").WithName("Basic");
+        Basic.Grouped(group);
+        group = builder.MapGroup("prefix").WithName("Basic");
+        Basic.GroupedWithPrefix(group);
+        return builder;
+    }
+
+    #endregion
+
+    public static void Main()
+    {
+        #region Usage
+
+        var builder = WebApplication.CreateBuilder();
+        var app = builder.Build();
+
+        app.MapEndpointGeneratorExampleEndpoints();
+
+        app.Run();
+
+        #endregion
+    }
+}
